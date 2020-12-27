70cb83a7a81878cc200050fdecb53092abc6bcc0
tslight <tslight@pm.com>
2020-12-27 16:33:44 +0000

    use percentage regex to check

--- lazygit.el
+++ lazygit.el
@@ -131,8 +131,7 @@ Results will be pretty printed in a buffer."
 (defun lazygit-process-filter (process output)
   (when (and output
              (not (string-match ".*already up to date.*" (downcase output)))
-             (not (string-match ".*receiving objects.*" (downcase output)))
-             (not (string-match ".*counting objects.*" (downcase output)))
+             (not (string-match ".*[0-9]+%.*" (downcase output)))
              (buffer-live-p (process-buffer process)))
     (display-buffer (process-buffer process))
     (with-current-buffer (process-buffer process)
